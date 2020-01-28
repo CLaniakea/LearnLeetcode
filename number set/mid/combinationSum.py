51b85b6a22723d6f64157ddc5fa4ffea3a487a17
Haibin <1586601249@qq.com>
2020-01-28 13:44:27 +0800

    add this answer with more print

--- number set/mid/combinationSum.py
+++ number set/mid/combinationSum.py
@@ -32,8 +32,6 @@ A solution set is:
 #         return False
 #     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
 #         pass
-#
-#
 # def permute(list, s):
 #     if list == 1:
 #         return s
@@ -42,28 +40,28 @@ A solution set is:
 #                  for x in permute(1, s)
 #                  for y in permute(list - 1, s)
 #                 ]
-#
-#
-#
-#
 # print(permute(1, ["a","b","c"]))
 # print(permute(3, ["a","b","c"]))
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         res = []
-        def backtrack(combination, candidates, target, start):
+        def backtrack(combination, candidates, target, start):# no start will generate repeated answers
             if not candidates:
                 return
             # if combination and target < combination[-1]:
             #     return
             if target < 0:
+                #print("target", target, "combination", combination)
                 return
             if 0 == target:
+                #print("target = 0 combination", combination)# test
                 res.append(combination)
                 return
             for i in range(start,len(candidates)):
-                print("i", i, "start", start, "combination", combination)
-                backtrack(combination + [candidates[i]], candidates, target - candidates[i], i)
+                #print("i", i, "start", start, "combination", combination)# test
+                #print("combination + [candidates[i]]", combination + [candidates[i]], "candidates", candidates, "target - candidates[i]", target - candidates[i], "i", i)
+                if candidates[i] <= target:
+                    backtrack(combination + [candidates[i]], candidates, target - candidates[i], i)
         backtrack([], candidates, target, 0)
         return res
 
@@ -101,5 +99,128 @@ if __name__ == '__main__':
     #solve = Solve()
     print(solution.combinationSum(nums,target))
     #print(solve.combinationSum(nums,target))
+'''
+i 0 start 0 combination []
+i 0 start 0 combination [2]
+i 0 start 0 combination [2, 2]
+i 0 start 0 combination [2, 2, 2]
+i 0 start 0 combination [2, 2, 2, 2]
+target = 0 combination [2, 2, 2, 2, 2]
 
 
+i 1 start 0 combination [2, 2, 2, 2]
+target -1 combination [2, 2, 2, 2, 3]
+
+i 2 start 0 combination [2, 2, 2, 2]
+target -4 combination [2, 2, 2, 2, 6]
+
+i 3 start 0 combination [2, 2, 2, 2]
+target -5 combination [2, 2, 2, 2, 7]
+
+
+i 1 start 0 combination [2, 2, 2]
+i 1 start 1 combination [2, 2, 2, 3]
+target -2 combination [2, 2, 2, 3, 3]
+
+i 2 start 1 combination [2, 2, 2, 3]
+target -5 combination [2, 2, 2, 3, 6]
+
+i 3 start 1 combination [2, 2, 2, 3]
+target -6 combination [2, 2, 2, 3, 7]
+
+
+i 2 start 0 combination [2, 2, 2]
+target -2 combination [2, 2, 2, 6]
+
+i 3 start 0 combination [2, 2, 2]
+target -3 combination [2, 2, 2, 7]
+
+
+i 1 start 0 combination [2, 2]
+i 1 start 1 combination [2, 2, 3]
+target = 0 combination [2, 2, 3, 3]
+
+i 2 start 1 combination [2, 2, 3]
+target -3 combination [2, 2, 3, 6]
+
+i 3 start 1 combination [2, 2, 3]
+target -4 combination [2, 2, 3, 7]
+
+
+i 2 start 0 combination [2, 2]
+target = 0 combination [2, 2, 6]
+
+i 3 start 0 combination [2, 2]
+target -1 combination [2, 2, 7]
+
+
+i 1 start 0 combination [2]
+i 1 start 1 combination [2, 3]
+i 1 start 1 combination [2, 3, 3]
+target -1 combination [2, 3, 3, 3]
+
+i 2 start 1 combination [2, 3, 3]
+target -4 combination [2, 3, 3, 6]
+
+i 3 start 1 combination [2, 3, 3]
+target -5 combination [2, 3, 3, 7]
+
+i 2 start 1 combination [2, 3]
+target -1 combination [2, 3, 6]
+
+i 3 start 1 combination [2, 3]
+target -2 combination [2, 3, 7]
+
+
+i 2 start 0 combination [2]
+i 2 start 2 combination [2, 6]
+target -4 combination [2, 6, 6]
+
+i 3 start 2 combination [2, 6]
+target -5 combination [2, 6, 7]
+
+
+i 3 start 0 combination [2]
+i 3 start 3 combination [2, 7]
+target -6 combination [2, 7, 7]
+
+
+i 1 start 0 combination []
+i 1 start 1 combination [3]
+i 1 start 1 combination [3, 3]
+i 1 start 1 combination [3, 3, 3]
+target -2 combination [3, 3, 3, 3]
+
+i 2 start 1 combination [3, 3, 3]
+target -5 combination [3, 3, 3, 6]
+
+i 3 start 1 combination [3, 3, 3]
+target -6 combination [3, 3, 3, 7]
+
+
+i 2 start 1 combination [3, 3]
+target -2 combination [3, 3, 6]
+
+i 3 start 1 combination [3, 3]
+target -3 combination [3, 3, 7]
+
+
+i 2 start 1 combination [3]
+i 2 start 2 combination [3, 6]
+target -5 combination [3, 6, 6]
+
+i 3 start 2 combination [3, 6]
+target -6 combination [3, 6, 7]
+
+
+i 3 start 1 combination [3]
+target = 0 combination [3, 7]
+i 2 start 0 combination []
+i 2 start 2 combination [6]
+target -2 combination [6, 6]
+i 3 start 2 combination [6]
+target -3 combination [6, 7]
+i 3 start 0 combination []
+i 3 start 3 combination [7]
+target -4 combination [7, 7]
+'''
